603fc9e6d9f71e0c02e048e1e6d331336f430b8d
Annisa Aulia Zahra <annisaauliazahra09@gmail.com>
2025-06-24 16:31:15 +0700

    Final: Setup routes crud

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\KomentarController;
+use App\Http\Controllers\PuisiController;
 use Illuminate\Support\Facades\Route;
 
 // Auth Route
@@ -9,3 +12,14 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// CRUD Route
+Route::resource('genres', GenreController::class)->middleware('auth');
+Route::resource('puisis', PuisiController::class);
+Route::post('/puisis/{puisi}/komentars', [KomentarController::class, 'store'])->name('komentars.store');
+Route::delete('/komentars/{komentar}', [KomentarController::class, 'destroy'])->name('komentars.destroy');
+
+// Home
+Route::get('/', function () {
+    return redirect()->route('puisis.index');
+});
