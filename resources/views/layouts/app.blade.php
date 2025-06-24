f17d385978ff7555999708385605d06fcba9cadf
Annisa Aulia Zahra <annisaauliazahra09@gmail.com>
2025-06-24 16:35:05 +0700

    Fix: add cdn bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Puisi App - @yield('title')</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-7ABi5O+1otJf9rN+dBdl/JwPTe4w1DS5cATD0IuMZbbmavU+Vf6M1vE3n5Udw2yG" crossorigin="anonymous">
 </head>
 
 <body class="d-flex flex-column min-vh-100">
@@ -22,6 +23,9 @@
     </main>
 
     @include('layouts.footer')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-vEN1h8yI7zwpJZ7UDFVlddvIHb0uZALz0aDmtZbNygJkR61ZJmZgSyQJihd4XLve" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
